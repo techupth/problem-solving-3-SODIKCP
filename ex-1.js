2c5105731bf12f856a4645702d5f81f8ab626f5c
Sodikcp <paramesh.rah@gmail.com>
2024-03-28 18:22:20 +0700

    finish

--- ex-1.js
+++ ex-1.js
@@ -2,6 +2,15 @@
 
 const lengthOfLastWord = function (s) {
   //Start coding here
+  let array = s.split(" ");
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]) {
+      newArray.push(array[i]);
+    }
+  }
+  let lastIndex = newArray.length-1
+  return newArray[lastIndex].length
 };
 
 const result1 = lengthOfLastWord("Hello World");
@@ -11,3 +20,5 @@ const result3 = lengthOfLastWord("luffy is still joyboy");
 console.log(result1); // 5
 console.log(result2); // 4
 console.log(result3); // 6
+
+
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,17 @@
 
 const singleNumber = function (nums) {
   // Start coding here
+  let count = {};
+  for (let i = 0; i < nums.length; i++) {
+    count[nums[i]] = (count[nums[i]] || 0) + 1;
+  }
+  console.log(count);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count[nums[i]] === 1) {
+      return nums[i]
+    }
+  }
 };
 
 const result1 = singleNumber([2, 2, 1]);
